8360f83aba980b397b35cc03836271fa43f70101
Yasemin Direk <yaseminndirek@gmail.com>
2022-05-05 18:54:02 +0300

    Data created.

--- CreateData.py
+++ CreateData.py
@@ -1,8 +1,15 @@
 import csv
 import random
+import pandas as pd
+import matplotlib.pyplot as plt
+from collections import Counter
+import numpy as np
+
+def random_num(first, last):
+    return round(random.uniform(first, last), 0)
 
 def random_float(first, last):
-    return round(random.uniform(first, last), 0)
+    return round(random.uniform(first, last), 1)
 
 def random_category(first, last):
     return "Category " + str(round(random.uniform(first, last), 0))
@@ -12,13 +19,20 @@ def write_to_csv(file_name, column_header, rowNumber):
         writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
         writer.writerow(column_header)
         for i in range(rowNumber):
-            correct_answer = random_float(201, 300)
-            answer_time = random_float(0, 20)
+            correct_answer = random_num(0, 10)
+            answer_time = random_float(130,140)
             row = [correct_answer, answer_time]
             writer.writerow(row)
 
 column_header = ['CorrectAnswer', 'AnswerTime']
 
-rowNumber = 500
+rowNumber = 10
 
-write_to_csv('data.csv',column_header,rowNumber)
+write_to_csv('test.csv',column_header,rowNumber)
+
+
+data = pd.read_csv('data.csv')
+# Plot raw data
+plt.scatter(data['CorrectAnswer'],data['AnswerTime'])
+
+plt.show()
--- LabelData.py
+++ LabelData.py
@@ -6,11 +6,17 @@ import numpy as np
 df = pd.read_csv("data.csv")
 print(df.head())
 
-df.loc[(df['CorrectAnswer']<=100.0) & (df['AnswerTime']>=45.0), 'classes'] = 'basarisiz'  #en basarisiz
+# df.loc[(df['CorrectAnswer']<=100.0) & (df['AnswerTime']>=45.0), 'classes'] = 'basarisiz'  #en basarisiz
+#
+# df.loc[(df['CorrectAnswer']>=101) & (df['CorrectAnswer']<=200.0) & (df['AnswerTime']>=25) & (df['AnswerTime']<=40.0), 'classes'] = 'orta'  #orta
+#
+# df.loc[(df['CorrectAnswer']>=200) & (df['CorrectAnswer']<=300.0) & (df['AnswerTime']>=0) & (df['AnswerTime']<=20.0), 'classes'] = 'basarili'   #basarili
 
-df.loc[(df['CorrectAnswer']>=101) & (df['CorrectAnswer']<=200.0) & (df['AnswerTime']>=25) & (df['AnswerTime']<=40.0), 'classes'] = 'orta'  #orta
+df.loc[(df['CorrectAnswer']<=100.0) , 'Degree'] = 'Unsuccessful'  #en basarisiz
 
-df.loc[(df['CorrectAnswer']>=201) & (df['CorrectAnswer']<=300.0) & (df['AnswerTime']>=0) & (df['AnswerTime']<=20.0), 'classes'] = 'basarili'   #basarili
+df.loc[(df['CorrectAnswer']>=100) & (df['CorrectAnswer']<=200.0) , 'Degree'] = 'Average'  #orta
+
+df.loc[(df['CorrectAnswer']>=200) & (df['CorrectAnswer']<=300.0) , 'Degree'] = 'Successful'   #basarili
 
 df.to_csv("data.csv", index=False)
 
